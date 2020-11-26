eda76457277438e0733f265b5164ee6a80b6c7ea
Jihyoung <emili1001@naver.com>
2020-11-26 18:45:26 +0900

    값 받아오는거 시도중...ㅜ

--- content/record/pickLastDate.js
+++ content/record/pickLastDate.js
@@ -6,33 +6,33 @@ var today = new Date();
 var today_year = today.getFullYear();
 var index = 0;
 for (var y = today_year; y >= start_year; y--) { //start_year ~ 현재 년도 
-    document.getElementById('select_year').options[index] = new Option(y, y); 
+    document.getElementById('select_lastyear').options[index] = new Option(y, y); 
     index++;
 }
 index = 0;
 for (var m = 1; m <= 12; m++) {
-    document.getElementById('select_month').options[index] = new Option(m, m);
+    document.getElementById('select_lastmonth').options[index] = new Option(m, m);
     index++;
 }
 
 
-function startday() { //년과 월에 따라 마지막 일 구하기 
-    var Year = document.getElementById('select_year').value;
-    var Month = document.getElementById('select_month').value;
+function lastday() { //년과 월에 따라 마지막 일 구하기 
+    var Year = document.getElementById('select_lastyear').value;
+    var Month = document.getElementById('select_lastmonth').value;
     var day = new Date(new Date(Year, Month, 1) - 86400000).getDate();
-    var dayindex_len = document.getElementById('select_day').length;
+    var dayindex_len = document.getElementById('select_lastday').length;
     if (day > dayindex_len) {
         for (var i = (dayindex_len + 1); i <= day; i++) {
-            document.getElementById('select_day').options[i - 1] = new Option(i, i);
+            document.getElementById('select_lastday').options[i - 1] = new Option(i, i);
         }
     }
     else if (day < dayindex_len) {
         for (var i = dayindex_len; i >= day; i--) {
-            document.getElementById('select_day').options[i] = null;
+            document.getElementById('select_lastday').options[i] = null;
         }
     }
 }
 
 function init(){
-    startday();
+    lastday();
 }
