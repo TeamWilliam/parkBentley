ff58b697bbd80b1bc2a77652874aa28eafe2a55a
zio <jiho8345@naver.com>
2020-11-28 00:22:41 +0900

    image preview

--- content/service/makeReservation.js
+++ content/service/makeReservation.js
@@ -1,4 +1,26 @@
-function printName()  {
-    const name = document.getElementById('name').value;
-    document.getElementById("result").innerText = name;
+var timeBlock = document.getElementsByClassName("timeBlock");
+
+function handleClick(event) {
+  console.log(event.target);
+  // console.log(this);
+  // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+  console.log(event.target.classList);
+
+  if (event.target.classList[1] === "clicked") {
+    event.target.classList.remove("clicked");
+  } else {
+    for (var i = 0; i < timeBlock.length; i++) {
+      timeBlock[i].classList.remove("clicked");
+    }
+
+    event.target.classList.add("clicked");
   }
+}
+
+function init() {
+  for (var i = 0; i < timeBlock.length; i++) {
+    timeBlock[i].addEventListener("click", handleClick);
+  }
+}
+init();
