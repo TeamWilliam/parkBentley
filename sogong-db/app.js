8ec0d6504bb705dd0b64269d1c05377e0a926073
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2020-11-28 05:36:42 +0900

    Update app.js

--- sogong-db/app.js
+++ sogong-db/app.js
@@ -35,7 +35,7 @@ app.get('/makeReservation', function (req, res) {
 
 /* -----------------------------개인 기록 조회--------------------------------------- */
 app.get('/myrecord', function (req, res) {
-    var sql = 'SELECT ReservationDate, StartTime, EndTime, ReservationNum, UseStatus FROM Reservation';
+    var sql = 'SELECT * FROM Reservation';
     conn.query(sql, function (err, rows, fields) {
         if(err) console.log('query is not excuted. select fail...\n' + err);
         else res.render('myrecord.ejs', {list : rows});
